68c4a715d0254adeb368883e495b071c32cf46a5
nOmeep <37767676+nOmeep@users.noreply.github.com>
2022-12-05 20:06:24 +0300

    thread(bruh) background calculations

--- app/src/main/java/com/sevastyan/ivfilters/filters/EffectManager.kt
+++ app/src/main/java/com/sevastyan/ivfilters/filters/EffectManager.kt
@@ -1,15 +1,21 @@
 package com.sevastyan.ivfilters.filters
 
+import android.os.Handler
+import android.os.HandlerThread
 import android.widget.ImageView
 
 internal abstract class EffectManager<M>(
     protected val model: M
 ) {
+    private val handlerThread: HandlerThread = HandlerThread("").apply { start() }
+    private val handler: Handler = Handler(handlerThread.looper)
+
     fun executeOn(imageView: ImageView) {
-        applyEffect(imageView)
-        imageView.invalidate()
+        handler.post {
+            applyEffect(imageView)
+            imageView.invalidate()
+        }
     }
 
-    // TODO: filter on background
     protected abstract fun applyEffect(imageView: ImageView)
 }
